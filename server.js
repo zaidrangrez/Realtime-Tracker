c279aba679b2f6f56fd6d555910db17fa23fef5e
zaidrangrez <z4ynxi@gmail.com>
2026-04-10 17:34:16 +0530

    Merged files and connection setup

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const app = require('./src/app.js')
 const http = require('http')
 const socketio = require('socket.io')
+const path = require('path')
+const express = require('express')
+
+const app = express()
 
 const server = http.createServer(app)
-const io = socketio()
+const io = socketio(server)
 
 app.set("view engine" , "ejs")
-app.set(express.static(path.join(__dirname,"public")))
+app.use(express.static("public"));
+io.on("connection", function(socket){
+    console.log('connected')
+})
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 server.listen(3000,()=>{
     console.log('server is running on port 3000')
