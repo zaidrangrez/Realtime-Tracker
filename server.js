97bd6f94b3917ba2d2866d8fb9ccdc9f316bd8c2
zaidrangrez <z4ynxi@gmail.com>
2026-04-10 16:42:28 +0530

    creating more needed files

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require('./src/app.js')
+const http = require('http')
+const socketio = require('socket.io')
 
-app.listen(3000,()=>{
+const server = http.createServer(app)
+const io = socketio()
+
+app.set("view engine" , "ejs")
+app.set(express.static(path.join(__dirname,"public")))
+
+server.listen(3000,()=>{
     console.log('server is running on port 3000')
 })
