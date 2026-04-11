0b8ce92fc9fe69953629cbe2fe5a272c36df626f
zaidrangrez <z4ynxi@gmail.com>
2026-04-11 12:36:20 +0530

    added comments

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,18 @@ const socket = io();
 if(navigator.geolocation){
     navigator.geolocation.watchPosition(
         (position)=>{
+            // getting coords
             const {latitude , longitude} = position.coords;
+
+            // handling socket 
             socket.emit("send-location" , {latitude,longitude})
-        },(error)=>{
+        },
+        // error handling  
+        (error)=>{
             console.error(error);
-        },{
+        },
+        // contomizing settings 
+        {
             enableHighAccuracy : true ,
             timeout : 5000,
             maximumAge : 0
@@ -17,25 +24,34 @@ if(navigator.geolocation){
     )
 }
 
+// setting up view 
 const map = L.map("map").setView([0,0] , 16)
 
+// creating a map layer 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
     atrribution : "@zaidrangrez"
 }).addTo(map)
 
+// creating markers 
 const markers = {}
 
+// handling recieved socket 
 socket.on("recieve-location" ,(data) => {
     const {id, longitude ,latitude} = data ;
     map.setView([latitude , longitude])
+
+    // if marker already exist
     if(markers[id]){
         markers[id].setLatLng([latitude,longitude])
-    }else{
+    }
+    // if marker didnt already exist 
+    else{
         markers[id] =  L.marker([latitude,longitude]).addTo(map)
     }
 })
 
 
+// handling disconnected users 
 socket.on("user-disconnected" , (id)=>{
     if(markers[id]){
         map.removeLayer(markers[id])
--- server.js
+++ server.js
@@ -1,29 +1,40 @@
+// Requiring everything
 const http = require('http')
 const socketio = require('socket.io')
-const path = require('path')
 const express = require('express')
-const { Socket } = require('dgram')
-
+const { disconnect } = require('cluster')
 const app = express()
-
+ 
+// creating server 
 const server = http.createServer(app)
+
+// creating a socket 
 const io = socketio(server)
 
+// setting up view engine 
 app.set("view engine" , "ejs")
+
+// setting up path 
 app.use(express.static("public"));
+
+// sending and receiving loaction req  
 io.on("connection", function(socket){
     socket.on("send-location" , function(data){
         io.emit("recieve-location",{id : socket.id , ...data})
     })
+
+    // handling disconnect users 
     socket.on("disconnected" ,function(){
         io.emit("user-disconnected", socket.id)
     })
 })
 
+// default api 
 app.get('/',(req,res)=>{
     res.render('index')
 })
 
+// server port 
 server.listen(3000,()=>{
     console.log('server is running on port 3000')
 })
