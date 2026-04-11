ca69930a0ea54217db4d69790929891033703080
zaidrangrez <z4ynxi@gmail.com>
2026-04-11 11:42:29 +0530

    Setting up map

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,25 @@
 const socket = io();
 
-console.log("first")
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(
+        (position)=>{
+            const {latitude , longitude} = position.coords;
+            socket.emit("send-location" , {latitude,longitude})
+        },(error)=>{
+            console.error(error);
+        },{
+            enableHighAccuracy : true ,
+            timeout : 5000,
+            maximumAge : 0
+        }
+            
+        
+    )
+}
+
+const map = L.map("map").setView([0,0] , 10)
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    atrribution : "@zaidrangrez"
+}).addTo(map)
+
